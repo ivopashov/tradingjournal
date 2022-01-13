9d8fb164ca63babf7db6d0dd53f1bb73abf76d69
Ivaylo Pashov <pashovi@amazon.com>
2022-01-13 10:37:50 +0000

    Add a bit more loggin in the scheduler

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -5,32 +5,33 @@ scheduler = Rufus::Scheduler.singleton
 
 scheduler.every '1m' do
     unless MarketsOpeningHours.open?
-        p "Skip DailyQuotes as it is weekend or holiday"
+        p "#{Time.now} Skip DailyQuotes as it is weekend or holiday"
         return
     end
 
     if MarketsOpeningHours.are_markets_regular_opening_hours_now? || MarketsOpeningHours.within_thirty_minutes_after_regular_hours_end?
-        p "Running daily quotes"
+        p "#{Time.now} Running daily quotes"
         DailyQuotes.delay.run
     else
-        p "Skip DailyQuotes as markets are closed"
+        p "#{Time.now} Skip DailyQuotes as markets are closed"
     end
 end
 
 scheduler.every '1m 30s' do
     unless MarketsOpeningHours.open?
-        p "Skip price alerts checking as it is weekend or holiday"
+        p "#{Time.now} Skip price alerts checking as it is weekend or holiday"
         return
     end
 
     if MarketsOpeningHours.are_markets_regular_opening_hours_now? || MarketsOpeningHours.within_thirty_minutes_after_regular_hours_end?
-        p "Checking price alerts"
+        p "#{Time.now} Checking price alerts"
         PriceAlertsChecker.delay.run
     else
-        p "Skip price alerts as markets are closed"
+        p "#{Time.now} Skip price alerts as markets are closed"
     end
 end
 
 scheduler.every '1h' do
+    p "#{Time.now} running StockSnapshotJanitor"
     StockSnapshotJanitor.delay.run
 end
