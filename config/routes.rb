862ccc3949b5ff2dc2e3bf92110a13512a56a87b
Ivaylo Pashov <pashovi@amazon.com>
2022-01-25 10:19:17 +0000

    Tweak what is available on Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "dashboard#index"
+  root "heat_maps#index"
 
-  post 'trades_import', to: 'trades#import'
-  get 'new_trades_import', to: 'trades#new_import'
-  resources :trades, only: [:new, :create, :edit, :update]
+  # post 'trades_import', to: 'trades#import'
+  # get 'new_trades_import', to: 'trades#new_import'
+  # resources :trades, only: [:new, :create, :edit, :update]
   resources :heat_maps
   resources :trading_alerts
-  resources :dashboard, only: [:show, :index]
+  # resources :dashboard, only: [:show, :index]
 end
